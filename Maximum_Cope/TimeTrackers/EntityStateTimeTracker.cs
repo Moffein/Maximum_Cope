de048f96241b6bf13ab52265066bebfe8431f161
Moffein <22742925+Moffein@users.noreply.github.com>
2024-08-29 13:19:07 -0700

    Improved things a bit?

--- Maximum_Cope/TimeTrackers/EntityStateTimeTracker.cs
+++ Maximum_Cope/TimeTrackers/EntityStateTimeTracker.cs
@@ -1,4 +1,7 @@
 ﻿using EntityStates;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -7,29 +10,63 @@ namespace Maximum_Cope.TimeTrackers
 {
     public static class EntityStateTimeTracker
     {
-        public static Dictionary<EntityState, float> lastDeltaUpdateDict = new Dictionary<EntityState, float>();
+        //public static Dictionary<EntityState, float> lastDeltaUpdateDict = new Dictionary<EntityState, float>();
         public static Dictionary<EntityState, float> lastUpdateDict = new Dictionary<EntityState, float>();
         public static Dictionary<EntityState, float> lastFixedUpdateDict = new Dictionary<EntityState, float>();
 
+        private const float frameTimeDefault = 1f / 60f;
+
         internal static void Init()
         {
-            On.EntityStates.EntityState.GetDeltaTime += EntityState_GetDeltaTime;
+            //On.EntityStates.EntityState.GetDeltaTime += EntityState_GetDeltaTime;
             On.EntityStates.EntityState.FixedUpdate += EntityState_FixedUpdate;
             On.EntityStates.EntityState.Update += EntityState_Update;
             RoR2.Stage.onStageStartGlobal += ClearNullEntries;
             On.EntityStates.EntityState.OnEnter += EntityState_OnEnter;
             On.EntityStates.EntityState.OnExit += EntityState_OnExit;
+
+            IL.EntityStates.BaseCharacterMain.UpdateAnimationParameters += ConvertToDeltaTime;
+            IL.EntityStates.GoldGat.GoldGatFire.Update += ConvertToDeltaTime;
+            IL.EntityStates.GoldGat.GoldGatIdle.Update += ConvertToDeltaTime;
         }
 
-        //Don't update lastDeltaUpdate here, because this can get called multiple times in 1 frame.
+        private static void ConvertToDeltaTime(MonoMod.Cil.ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+            if (c.TryGotoNext(MoveType.After, x => x.MatchCall(typeof(EntityState), "GetDeltaTime")))
+            {
+                c.Emit(OpCodes.Ldarg_0);
+                c.EmitDelegate<Func<float, EntityState, float>> ((deltaTime, self) => {
+                    if (lastUpdateDict.TryGetValue(self, out float lastUpdateTime))
+                    {
+                        deltaTime = Time.time - lastUpdateTime;
+                    }
+                    return deltaTime;
+                });
+            }
+            else
+            {
+                Debug.LogError("Maximum Cope: Failed to IL Hook ConvertToDeltaTime");
+            }
+        }
+
         private static float EntityState_GetDeltaTime(On.EntityStates.EntityState.orig_GetDeltaTime orig, EntityState self)
         {
-            if (lastDeltaUpdateDict.TryGetValue(self, out float lastUpdateTime))
+            //Don't update lastDeltaUpdate here, because this can get called multiple times in 1 frame.
+            /*if (lastDeltaUpdateDict.TryGetValue(self, out float lastUpdateTime))
             {
                 float deltaTime = Time.time - lastUpdateTime;
-                lastDeltaUpdateDict[self] = Time.time;
+                //lastDeltaUpdateDict[self] = Time.time;
                 return deltaTime;
+            }*/
+
+            //What if we just use FixedDeltaTime here?
+            if (lastFixedUpdateDict.TryGetValue(self, out float lastUpdateTime))
+            {
+                float deltaTime = Time.time - lastUpdateTime;
+                if (deltaTime <= 0f) deltaTime = frameTimeDefault;  //if called at the start of a state, things can break if this isn't set.
             }
+
             return orig(self);
         }
 
@@ -57,9 +94,9 @@ namespace Maximum_Cope.TimeTrackers
 
         private static void ClearNullEntries(RoR2.Stage obj)
         {
-            lastDeltaUpdateDict = (from kv in lastDeltaUpdateDict
+            /*lastDeltaUpdateDict = (from kv in lastDeltaUpdateDict
                                    where kv.Key != null
-                                   select kv).ToDictionary(kv => kv.Key, kv => kv.Value);
+                                   select kv).ToDictionary(kv => kv.Key, kv => kv.Value);*/
             lastUpdateDict = (from kv in lastUpdateDict
                                    where kv.Key != null
                                    select kv).ToDictionary(kv => kv.Key, kv => kv.Value);
@@ -71,14 +108,14 @@ namespace Maximum_Cope.TimeTrackers
         private static void EntityState_OnExit(On.EntityStates.EntityState.orig_OnExit orig, EntityState self)
         {
             orig(self);
-            lastDeltaUpdateDict.Remove(self);
+            //lastDeltaUpdateDict.Remove(self);
             lastUpdateDict.Remove(self);
             lastFixedUpdateDict.Remove(self);
         }
 
         private static void EntityState_OnEnter(On.EntityStates.EntityState.orig_OnEnter orig, EntityState self)
         {
-            lastDeltaUpdateDict.Add(self, Time.time);
+            //lastDeltaUpdateDict.Add(self, Time.time);
             lastUpdateDict.Add(self, Time.time);
             lastFixedUpdateDict.Add(self, Time.time);
             orig(self);
